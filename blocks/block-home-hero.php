872259eb6fe22b2ec9e1a4e3f9b9500f128236ee
Martins <martins@skywebdigital.com>
2023-12-14 22:20:42 +0200

    Fix Home hero image url

--- blocks/block-home-hero.php
+++ blocks/block-home-hero.php
@@ -12,75 +12,50 @@
 <?php if ($show_home_hero): ?>
     <div class="block-hero">
         <?php if ($home_hero_slide_one['home_hero_img'] && $home_hero_slide_one['home_hero_title']):
-            $s_img = $home_hero_slide_one['home_hero_img'];
-            $s_image_src = wp_get_attachment_image_src($s_img, 'full');
-            $s_image_alt = get_post_meta($s_img, '_wp_attachment_image_alt', true);
             $s_title = $home_hero_slide_one['home_hero_title'];
             ?>
             <div class="slide">
-                <?php if ($s_img): ?>
-                    <div class="slide-img" style="background: url(<?= $s_image_src[0]; ?>) no-repeat center top/cover"></div>
-                <?php endif; ?>
+                <div class="slide-img" style="background: url(<?= $home_hero_slide_one['home_hero_img']; ?>) no-repeat center top/cover"></div>
                 <?php if ($s_title): ?>
                     <h2><?= $s_title ?></h2>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
         <?php if ($home_hero_slide_two['home_hero_img'] && $home_hero_slide_two['home_hero_title']):
-            $s_img = $home_hero_slide_two['home_hero_img'];
-            $s_image_src = wp_get_attachment_image_src($s_img, 'full');
-            $s_image_alt = get_post_meta($s_img, '_wp_attachment_image_alt', true);
             $s_title = $home_hero_slide_two['home_hero_title'];
             ?>
             <div class="slide">
-                <?php if ($s_img): ?>
-                    <div class="slide-img" style="background: url(<?= $s_image_src[0]; ?>) no-repeat center top/cover"></div>
-                <?php endif; ?>
+                <div class="slide-img" style="background: url(<?= $home_hero_slide_two['home_hero_img']; ?>) no-repeat center top/cover"></div>
                 <?php if ($s_title): ?>
                     <h2><?= $s_title ?></h2>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
         <?php if ($home_hero_slide_three['home_hero_img'] && $home_hero_slide_three['home_hero_title']):
-            $s_img = $home_hero_slide_three['home_hero_img'];
-            $s_image_src = wp_get_attachment_image_src($s_img, 'full');
-            $s_image_alt = get_post_meta($s_img, '_wp_attachment_image_alt', true);
             $s_title = $home_hero_slide_three['home_hero_title'];
             ?>
             <div class="slide">
-                <?php if ($s_img): ?>
-                    <div class="slide-img" style="background: url(<?= $s_image_src[0]; ?>) no-repeat center top/cover"></div>
-                <?php endif; ?>
+                <div class="slide-img" style="background: url(<?= $home_hero_slide_three['home_hero_img']; ?>) no-repeat center top/cover"></div>
                 <?php if ($s_title): ?>
                     <h2><?= $s_title ?></h2>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
         <?php if ($home_hero_slide_four['home_hero_img'] && $home_hero_slide_four['home_hero_title']):
-            $s_img = $home_hero_slide_four['home_hero_img'];
-            $s_image_src = wp_get_attachment_image_src($s_img, 'full');
-            $s_image_alt = get_post_meta($s_img, '_wp_attachment_image_alt', true);
             $s_title = $home_hero_slide_four['home_hero_title'];
             ?>
             <div class="slide">
-                <?php if ($s_img): ?>
-                    <div class="slide-img" style="background: url(<?= $s_image_src[0]; ?>) no-repeat center top/cover"></div>
-                <?php endif; ?>
+                <div class="slide-img" style="background: url(<?= $home_hero_slide_four['home_hero_img']; ?>) no-repeat center top/cover"></div>
                 <?php if ($s_title): ?>
                     <h2><?= $s_title ?></h2>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
         <?php if ($home_hero_slide_five['home_hero_img'] && $home_hero_slide_five['home_hero_title']):
-            $s_img = $home_hero_slide_five['home_hero_img'];
-            $s_image_src = wp_get_attachment_image_src($s_img, 'full');
-            $s_image_alt = get_post_meta($s_img, '_wp_attachment_image_alt', true);
             $s_title = $home_hero_slide_five['home_hero_title'];
             ?>
             <div class="slide">
-                <?php if ($s_img): ?>
-                    <div class="slide-img" style="background: url(<?= $s_image_src[0]; ?>) no-repeat center top/cover"></div>
-                <?php endif; ?>
+                <div class="slide-img" style="background: url(<?= $home_hero_slide_five['home_hero_img']; ?>) no-repeat center top/cover"></div>
                 <?php if ($s_title): ?>
                     <h2><?= $s_title ?></h2>
                 <?php endif; ?>
